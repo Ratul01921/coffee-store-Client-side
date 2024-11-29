dbfb8a4694ea2d2d42cc99b9f6d8209c26f0839c
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-11-29 17:14:08 +0600

    first commit

---
+++ src/Components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <p className='btn'>update coffee</p>
+        </div>
+    );
+};
+
+export default UpdateCoffee;<p>update coffee</p>
