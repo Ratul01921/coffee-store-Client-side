561316786f43edc14fb96dd5849a98cbd1aa62dd
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-11-30 23:10:40 +0600

    coffee card delete and update section complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './Components/CoffeeCard';
+import { useState } from 'react';
 
 function App() {
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
   // console.log(loadedCoffee)
+  const [coffees, setCoffees] = useState(loadedCoffees);
 
 
   return (
     <>
 
       <h1 className='text-6xl text-purple-700' >Hit Hot Coffee: {coffees.length}</h1>
-      <div  className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+      <div className='w-10/12 mx-auto mt-10 grid grid-cols-1 lg:grid-cols-2 gap-8'>
         {
-          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+
+          ></CoffeeCard>)
         }
       </div>
 
--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,33 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
-const CoffeeCard = ({ coffee}) => {
-    const { _id, name,  quantity, taste, photo } = coffee;
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+    const { _id, name, quantity, taste, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire({
+                //   title: "Deleted!",
+                //   text: "Your file has been deleted.",
+                //   icon: "success"
+                // });
+                console.log('delete confirmed')
+                fetch(`http://localhost:5555/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Coffee has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = coffees.filter(cof => cof._id !== _id)
+                            setCoffees(remaining)
+                        }
+
+                    })
+            }
+        });
+    }
     return (
         <div>
             <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure>
-                <img
-                    src={photo}
-                    alt="coffee" />
-            </figure>
-            <div className="flex w-full m-4 items-center justify-between">
-                <div>
-                    <p>Name: {name}</p>
-                    <p>Quantity: {quantity}</p>
-                    <p>Taste: {taste}</p>
-                </div>
-                <div className="card-actions justify-end join join-vertical">
-                    <button className="btn join-item">View</button>
-                    <Link to={`/updateCoffee/${_id}`}>
-                        <button className="btn join-item">Edit</button>
-                    </Link>
-                    <button
-                        // onClick={() => handleDelete(_id)}
-                        className="btn join-item bg-red-500">X</button>
+                <figure>
+                    <img
+                        src={photo}
+                        alt="coffee" />
+                </figure>
+                <div className="flex w-full m-4 items-center justify-between">
+                    <div>
+                        <p>Name: {name}</p>
+                        <p>Quantity: {quantity}</p>
+                        <p>Taste: {taste}</p>
+                    </div>
+                    <div className="card-actions justify-end join join-vertical">
+                        <button className="btn join-item">View</button>
+                        <Link to={`/updateCoffee/${_id}`}>
+                            <button className="btn join-item">Edit</button>
+                        </Link>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn join-item bg-red-500">X</button>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };
